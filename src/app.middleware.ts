d09832ba9f45ddb354ee1acb659fe8e50348de1e
Adrian Abadin <aabadin@gmail.com>
2024-06-05 12:45:53 -0300

    Fixing cookies

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -6,13 +6,9 @@ import './auth/localStrategy.module'
 import './auth/googleOauth2.module'
 import './auth/jwtStrategy.module'
 import './auth/facebook.module'
-
 import { routeHandler } from './app.routes'
 import cors from 'cors'
-
 import morgan from 'morgan'
-
-// const authService = new AuthService()
 export const app = express()
 
 app.use(express.json())
@@ -26,26 +22,5 @@ app.use(cors({
   preflightContinue: true
 }))
 app.use(express.static('public'))
-
-// const store = new PrismaSessionStore(prismaClient, {
-//   checkPeriod: 2 * 60 * 1000, // ms
-//   dbRecordIdIsSessionId: true,
-//   dbRecordIdFunction: undefined,
-//   ttl: 60 * 60 * 1000 * 24
-// })
-// const sessionMiddleware = Session({
-
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { sameSite: 'none', secure: true, httpOnly: false },
-//   secret: 'Dilated flakes of fire fall, like snow in the Alps when there is no wind'
-
-// })
-
-// app.use(sessionMiddleware)
-// app.use(flash())
 app.use(passport.initialize())
-// app.use(passport.session())
-// passport.serializeUser(authService.serialize)
-// passport.deserializeUser(authService.deSerialize)
 routeHandler(app)
